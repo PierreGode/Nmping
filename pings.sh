24e4988f6027d11262560e380c7f1b6bd63994f0
PierreGoude <linoge80@live.se>
2016-12-15 13:47:15 +0100

    Update pings.sh

--- pings.sh
+++ pings.sh
@@ -1,54 +1,19 @@
 #!/bin/bash
 ########################### Start ################################
 #                                                                #
-#   Start ping to google and then send to servers or to loop     #
+#  ping, insert iplist and check   #
 #                                                                #
 ##################################################################
-start(){
-content=$(cat logs/pinglog.log)
-if [ $content = 2 ]
-then
-break;
-else
-IP='www.google.se'
-fping -c1 -t300 $IP 2>/dev/null 1>/dev/null
-  if [ "$?" = 0 ]
-then
-  echo "1" > logs/pinglog.log
-servers
-else
-controll
-  fi
-fi
-}
-controll(){
-sleep 7
-IP='www.google.se'
-fping -c1 -t300 $IP 2>/dev/null 1>/dev/null
-  if [ "$?" = 0 ]
-then
-  echo "1" > logs/pinglog.log
-servers
-else
-pico2wave -w logs/ping.wav "Hello technical operations., I have connection troubles" && aplay logs/ping.wav
-loop
-  fi
-}
-############################# loop ###############################
-#                                                                #
-#               Loop loops pings google till success             #
-#                                                                #
-##################################################################
-loop(){
-sleep 10
-IP='www.google.com'
-fping -c1 -t3000 $IP 2>/dev/null 1>/dev/null
-if [ "$?" = 0 ]
-then
-pico2wave -w logs/ping1.wav "hurray. We have network connection again" && aplay logs/ping1.wav 
-echo "1" > pinglog.log
-else
-loop
-fi
-}
-start
+echo "Type in path or file with IP adresse you wish to check"
+read IPlisT
+date
+cat $IPlisT | while read output
+do
+    ping "$output" > /dev/null
+    if [ $? -eq 0 ]; then
+    echo "$output is up" 
+    else
+    echo "$output is down"
+    fi
+done
+
