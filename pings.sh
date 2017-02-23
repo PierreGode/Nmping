118f9a961d235efc4b859299b8ca6bc7d5496305
PierreGoude <pierre@webbhatt.com>
2017-02-23 12:50:52 +0100

    Update pings.sh

--- pings.sh
+++ pings.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 ########################### Start ################################
 #                                                                #
-#  ping, insert iplist and check   #
+#     create ip.lst and add a list of ip adresses to check       #
 #                                                                #
 ##################################################################
-echo "Type in path or file with IP adresse you wish to check"
-read IPlisT
-date
-cat $IPlisT | while read output
+
+cat ip.lst | while read output
 do
-    ping -c 4 "$output" > /dev/null
+    ping -c 1 "$output" > /dev/null
     if [ $? -eq 0 ]; then
     echo "$output is up" 
     else
