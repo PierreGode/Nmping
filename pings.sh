b75c42b2814dd70634f7e2b5a655c1472e2f96de
PierreGoude <pierre@webbhatt.com>
2018-01-03 12:39:36 +0100

    Update pings.sh

--- pings.sh
+++ pings.sh
@@ -6,8 +6,9 @@
 ##################################################################
 
 echo "${MENU}"Checking network"${END}"
-ifconfig | awk '{print $2}' | grep addr | head -1
+yourip=$(ip a | grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | grep -v 127 | awk '{print $2}' | cut -d '/' -f1)
 speed=$( ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` | grep max | awk '{print$4}' | cut -d '/' -f1)
+echo your IP is $yourip
 echo "$speed ms"
 ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo "Network is working" || echo "NO Network"
 
