d25ac4aa2c9dcad2421392b67441afa7edc4e4c4
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-05-22 10:19:02 +0200

    Update Nmping.py

--- Nmping.py
+++ Nmping.py
@@ -29,9 +29,11 @@ class App:
         self.info_button.grid(row=0, column=4, padx=5, pady=5, sticky='e')
 
         self.treeview = ttk.Treeview(self.window)
-        self.treeview["columns"] = ("status", "ports")
+        self.treeview["columns"] = ("hostname", "status", "ports")
         self.treeview.heading("#0", text="IP Address", command=lambda: self.sort_column("#0", False))
         self.treeview.column("#0", width=150)
+        self.treeview.heading("hostname", text="Hostname")
+        self.treeview.column("hostname", width=150)
         self.treeview.heading("status", text="Status")
         self.treeview.column("status", width=100)
         self.treeview.heading("ports", text="Open Ports")
@@ -58,21 +60,16 @@ class App:
 
     def check_queue(self):
         while not self.result_queue.empty():
-            ip, status, open_ports_str = self.result_queue.get()
-            self.treeview.insert("", "end", text=ip, values=(status, open_ports_str))
+            ip, hostname, status, open_ports_str = self.result_queue.get()
+            self.treeview.insert("", "end", text=ip, values=(hostname, status, open_ports_str))
         self.window.after(100, self.check_queue)
 
     def ping_ips(self):
-        ip_input = self.ip_range_entry.get().strip()
-
-        if not ip_input:
-            messagebox.showwarning("Validation Error", "Please enter an IP address or range.")
-            return
-
+        ip_input = self.ip_range_entry.get()
         try:
             ip_list = self.parse_ip_input(ip_input)
         except ValueError as e:
-            messagebox.showwarning("Validation Error", str(e))
+            messagebox.showerror("Validation Error", str(e))
             return
 
         for child in self.treeview.get_children():
@@ -95,39 +92,27 @@ class App:
 
     def parse_ip_input(self, ip_input):
         if "-" in ip_input:
-            start_ip, end_ip = ip_input.split("-")
-            try:
-                ipaddress.ip_address(start_ip)
-                if '.' in end_ip or ':' in end_ip:
-                    ipaddress.ip_address(end_ip)
-            except ValueError:
-                raise ValueError("Invalid IP address in range.")
-            if '.' in start_ip:  # IPv4
-                start_parts = start_ip.split(".")
-                end_parts = end_ip if "." in end_ip else start_parts[:-1] + [end_ip]
-                return [str(ipaddress.ip_address(f"{start_parts[0]}.{start_parts[1]}.{start_parts[2]}.{i}")) for i in range(int(start_parts[3]), int(end_parts[3]) + 1)]
-            else:  # IPv6
-                start_parts = start_ip.split(":")
-                end_parts = end_ip if ":" in end_ip else start_parts[:-1] + [end_ip]
-                # The range for IPv6 could be very large, so you might want to limit the range or improve this logic
-                return [str(ipaddress.ip_address(f"{':'.join(start_parts[:-1])}:{i}")) for i in range(int(start_parts[-1], 16), int(end_parts[-1], 16) + 1)]
-        elif "/" in ip_input:
-            try:
-                return [str(ip) for ip in ipaddress.ip_network(ip_input)]
-            except ValueError:
-                raise ValueError("Invalid IP network.")
+            ip_range = ip_input.split("-")
+            ip_list = [str(ip) for ip in ipaddress.ip_network(ip_range[0]).hosts()]
         else:
-            try:
-                return [str(ipaddress.ip_address(ip_input))]
-            except ValueError:
-                raise ValueError("Invalid IP address.")
+            ip_list = [ip_input]
+        return ip_list
 
     def ping_ip(self, ip):
+        nslookup_cmd = ["nslookup", ip]
+        nslookup_process = run(nslookup_cmd, capture_output=True, text=True)
+
+        hostname = None
+        for line in nslookup_process.stdout.splitlines():
+            if "name" in line.lower():
+                hostname = line.split("=")[-1].strip()
+                break
+
         ping_cmd = self.get_ping_cmd(ip)
         ping_process = run(ping_cmd, capture_output=True, text=True, timeout=5)
 
         if "Destination host unreachable" in ping_process.stdout:
-            self.result_queue.put((ip, "Down", "N/A"))
+            self.result_queue.put((ip, hostname, "Down", "N/A"))
             return
 
         is_alive = "1 packets received" in ping_process.stdout or "Received = 1" in ping_process.stdout
@@ -146,27 +131,18 @@ class App:
             open_ports_str = "N/A"
             status = "Down"
 
-        self.result_queue.put((ip, status, open_ports_str))
+        self.result_queue.put((ip, hostname, status, open_ports_str))
 
     @staticmethod
     def get_ping_cmd(ip):
-        current_platform = platform.system().lower()
-        return ["ping", "-c", "1", ip] if current_platform != "windows" else ["ping", "-n", "1", ip]
+        if platform.system().lower() == "windows":
+            return ["ping", "-n", "1", "-w", "1000", ip]
+        else:
+            return ["ping", "-c", "1", "-W", "1", ip]
 
     def show_info(self):
-        info_text = "NmPing\n\n" \
-                    "A tool for pinging and port scanning multiple IP addresses.\n\n" \
-                    "Usage:\n" \
-                    "1. Enter an IP address or range in the IP Range field.\n" \
-                    "2. Click on the Ping button to start pinging.\n" \
-                    "3. The tool will display the status and open ports for each IP address.\n" \
-                    "4. Use the Stop button to stop the pinging process.\n\n" \
-                    "Note:\n" \
-                    "- IP range can be specified in the following formats:\n" \
-                    "  - Single IP: e.g., 192.168.1.100 or 2001:db8::1\n" \
-                    "  - IP range: e.g., 192.168.1.100-150 or 2001:db8::1-10\n" \
-                    "  - Subnet: e.g., 192.168.1.0/24 or 2001:db8::/32"
-        messagebox.showinfo("NmPing - Usage", info_text)
+        messagebox.showinfo("Info", "This tool pings a range of IPs, performs an NSLOOKUP, and lists any open ports.")
+
 
 if __name__ == "__main__":
     App()
